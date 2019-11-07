fada676f5abeeb8c8f04b341edd2a503fe9d326f
jamievullo <jvullo@protonmail.com>
2019-11-07 16:31:48 -0500

    Added links to navbar and corresponding routes and views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  root 'home#index'
+  root 'home#welcome'
   
+  get '/about_us', to: 'home#about_us'
+
+  get '/buying_tools', to: 'home#buying_tools'
+
   resources :drivers
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
